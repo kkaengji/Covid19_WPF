ba62ab5ff7a958dc37f82cff601f9757ff203c47
kkaengji <nkj960425@naver.com>
2020-09-08 16:44:39 +0900

    디자인 작업

---
+++ Covid19/Covid19/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using Covid19.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Covid19
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ Covid19/Covid19/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid19.ViewModels
+{
+    class MainViewModel : Conductor<object>, IHaveDisplayName
+    {
+        public MainViewModel()
+        {
+            ActivateItem(new KoreaViewModel());
+        }
+
+        #region 버튼 클릭 이벤트
+
+        public void KoreaTab()
+        {
+            ActivateItem(new KoreaViewModel());
+        }
+
+        public void ForeignCountryTab()
+        {
+            ActivateItem(new ForeignCountryViewModel());
+        }
+
+        #endregion
+    }
+}
